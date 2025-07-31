5bca5b034d47c1adeccaea9187a4d4f56c5d49be
Amirkianfar66 <amir.kianfar@limenet.tech>
2025-07-31 09:56:45 +0200

    Adding new files

---
+++ src/Icons/Equipment.jsx
@@ -0,0 +1,9 @@
+const EquipmentIcon = ({ size = 60 }) => (
+  <svg width={size} height={size} viewBox="0 0 64 64" fill="none">
+    <rect width="64" height="64" rx="8" fill="#4CAF50" />
+    <text x="50%" y="50%" dominantBaseline="middle" textAnchor="middle" fill="white" fontSize="14">
+      EQ
+    </text>
+  </svg>
+);
+export default EquipmentIcon;
---
+++ src/Icons/Index.jsx
@@ -0,0 +1,15 @@
+import Equipment from './Equipment';
+import Instrument from './Instrument';
+import Pipe from './Pipe';
+import InlineValve from './InlineValve';
+import Electrical from './Electrical';
+
+const CategoryIcons = {
+  Equipment,
+  Instrument,
+  Pipe,
+  'Inline Valve': InlineValve,
+  Electrical,
+};
+
+export default CategoryIcons;
