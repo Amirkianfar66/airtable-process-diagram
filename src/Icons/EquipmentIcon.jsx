7bafd7015baca3e29459449b0d2430cc0dcd0441
Amirkianfar66 <amir.kianfar@limenet.tech>
2025-08-06 17:17:32 +0200

    Update EquipmentIcon.jsx

--- src/Icons/EquipmentIcon.jsx
+++ src/Icons/EquipmentIcon.jsx
@@ -1,10 +1,10 @@
-// src/Icons//EquipmentIcon.jsx
+// src/Icons/EquipmentIcon.jsx
 import React from 'react';
 
 export default function EquipmentIcon() {
   return (
-    <svg width="50" height="50" viewBox="0 0 100 100" fill="none">
-      <rect x="20" y="20" width="400" height="400" fill="green" stroke="black" strokeWidth="4" />
+    <svg width="500" height="500" viewBox="0 0 100 100" fill="none">
+      <rect x="20" y="20" width="60" height="60" fill="green" stroke="black" strokeWidth="4" />
       <text x="50" y="55" fontSize="16" textAnchor="middle" fill="white">EQ</text>
     </svg>
   );
