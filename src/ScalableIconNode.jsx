b2983051107434826e6418b9cf17c0bb4f73353d
Amirkianfar66 <amir.kianfar@limenet.tech>
2025-08-07 15:49:23 +0200

    d

--- src/ScalableIconNode.jsx
+++ src/ScalableIconNode.jsx
@@ -1,4 +1,4 @@
-﻿:// ScalableIconNode.jsx
+﻿// ScalableIconNode.jsx
 import React, { useState, useEffect, useRef } from 'react';
 import { Handle, Position, useReactFlow } from 'reactflow';
 
@@ -62,7 +62,7 @@ export default function ScalableIconNode({ id, data }) {
             <div
                 ref={iconRef}
                 style={{
-                    transform: scale(${ scaleX }, ${ scaleY }),
+                    transform: `scale(${scaleX}, ${scaleY})`,
                     transformOrigin: 'top left',
                     width: baseSize,
                     height: baseSize,
@@ -100,26 +100,24 @@ export default function ScalableIconNode({ id, data }) {
                 type="target"
                 position={Position.Left}
                 style={{
-                    top: ${height / 2} px,
-            left: 0px,
-            transform: 'translate(-50%, -50%)',
-            pointerEvents: 'auto',
-            position: 'absolute'
+                    top: `${height / 2}px`,
+                    left: `0px`,
+                    transform: 'translate(-50%, -50%)',
+                    pointerEvents: 'auto',
+                    position: 'absolute'
                 }}
             />
             <Handle
                 type="source"
                 position={Position.Right}
                 style={{
-                    top: ${height / 2} px,
-            left: ${width}px,
-            transform: 'translate(-50%, -50%)',
-            pointerEvents: 'auto',
-            position: 'absolute'
+                    top: `${height / 2}px`,
+                    left: `${width}px`,
+                    transform: 'translate(-50%, -50%)',
+                    pointerEvents: 'auto',
+                    position: 'absolute'
                 }}
             />
         </div>
     );
 }
-
-
