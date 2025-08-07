11f228212ce16f4f2fbbfee98986af6e4271764c
Amirkianfar66 <amir.kianfar@limenet.tech>
2025-08-07 15:57:57 +0200

    s

--- src/Icons/EquipmentIcon.jsx
+++ src/Icons/EquipmentIcon.jsx
@@ -1,40 +1,59 @@
-// src/Icons/EquipmentIcon.jsx
 import React from 'react';
 import { Handle, Position } from 'reactflow';
 
-export default function EquipmentIcon() {
-    return (
-        <div style={{ position: 'relative', width: 100, height: 100 }}>
-            <svg width="100" height="100" viewBox="0 0 100 100" fill="none">
-                <rect x="20" y="20" width="60" height="60" fill="green" stroke="black" strokeWidth="4" />
-                <text x="50" y="55" fontSize="16" textAnchor="middle" fill="white">EQ</text>
-            </svg>
+export default function EquipmentIcon({ scaleX = 1, scaleY = 1 }) {
+    const rectLeft = 20 * scaleX;
+    const rectRight = (20 + 60) * scaleX;
+    const rectTopMid = 20 * scaleY + (60 * scaleY) / 2;
 
-            {/* Left handle on left edge of rect (x=20) */}
+    return (
+        <div style={{ position: 'relative', width: 100 * scaleX, height: 100 * scaleY }}>
+            {/* Scaled SVG */}
+            <div
+                style={{
+                    transform: `scale(${scaleX}, ${scaleY})`,
+                    transformOrigin: 'top left',
+                    width: 100,
+                    height: 100,
+                }}
+            >
+                <svg width="100" height="100" viewBox="0 0 100 100" fill="none">
+                    <rect x="20" y="20" width="60" height="60" fill="green" stroke="black" strokeWidth="4" />
+                    <text x="50" y="55" fontSize="16" textAnchor="middle" fill="white">EQ</text>
+                </svg>
+            </div>
+
+            {/* Handles NOT scaled */}
             <Handle
                 type="target"
                 position={Position.Left}
                 id="left"
                 style={{
                     position: 'absolute',
-                    top: '50%',
-                    left: 20,
+                    top: rectTopMid,
+                    left: rectLeft,
                     transform: 'translate(-50%, -50%)',
-                    background: 'red', // optional, for visibility
+                    width: 12,
+                    height: 12,
+                    background: 'red',
+                    borderRadius: '50%',
+                    zIndex: 10,
                 }}
             />
-
-            {/* Right handle on right edge of rect (x=20+60=80) */}
             <Handle
                 type="source"
                 position={Position.Right}
                 id="right"
                 style={{
                     position: 'absolute',
-                    top: '50%',
-                    left: 80,
+                    top: rectTopMid,
+                    left: rectRight,
                     transform: 'translate(-50%, -50%)',
-                    background: 'blue', // optional, for visibility
+                    width: 12,
+                    height: 12,
+                    background: 'blue',
+                    borderRadius: '50%',
+                    zIndex: 10,
                 }}
             />
         </div>
