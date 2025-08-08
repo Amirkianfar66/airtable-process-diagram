ef082811591bbb76c56ee73ceb09231b069a7e70
Amirkianfar66 <amir.kianfar@limenet.tech>
2025-08-08 14:51:24 +0200

    fdddf

--- src/Icons/InstrumentIcon.jsx
+++ src/Icons/InstrumentIcon.jsx
@@ -30,7 +30,7 @@ export default function InstrumentIcon({ data }) {
                 type="target"
                 position={Position.Bottom}
                 style={{
-                    left: '80%',
+                    left: '95%',
                     background: 'green',
                     border: '1px solid white',
                     borderRadius: '50%',
