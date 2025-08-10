5a966c29996a7adf03e50ea3d9ca52f72ae8b0ac
Amirkianfar66 <amir.kianfar@limenet.tech>
2025-08-10 10:15:20 +0200

    8i

--- src/Icons/EquipmentIcon.jsx
+++ src/Icons/EquipmentIcon.jsx
@@ -70,7 +70,7 @@ export default function EquipmentIcon({ id, data }) {
                 <svg
                     width="100"
                     height="100"
-                    viewBox="0 0 200 200"
+                    viewBox="0 0 150 150"
                     style={{
                         pointerEvents: 'none',
                         display: 'block',
