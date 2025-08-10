bd80576c44b87f99ca212b0319764971816e7ed0
Amirkianfar66 <amir.kianfar@limenet.tech>
2025-08-10 11:30:58 +0200

    9iii

--- src/Icons/InlineValveIcon.jsx
+++ src/Icons/InlineValveIcon.jsx
@@ -61,7 +61,7 @@ export default function InlineValveIcon({ data }) {
                 position={Position.Left}
                 id="left"
                 style={{
-                    top: 30,
+                    top: 0,
                     left: -1,
                     width: 2,
                     height: 2,
@@ -77,7 +77,7 @@ export default function InlineValveIcon({ data }) {
                 position={Position.Right}
                 id="right"
                 style={{
-                    top: 30,
+                    top: 0,
                     right: -1,
                     width: 2,
                     height: 2,
