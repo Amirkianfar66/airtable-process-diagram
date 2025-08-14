7946e4a21d9d761ac856ab9c2c090a7f287b56e8
Amirkianfar66 <amir.kianfar@limenet.tech>
2025-08-15 00:11:35 +0200

    dd33332

--- src/ProcessDiagram.jsx
+++ src/ProcessDiagram.jsx
@@ -167,12 +167,29 @@ export default function ProcessDiagram() {
                     edges={edges}
                     onNodesChange={onNodesChange}
                     onEdgesChange={onEdgesChange}
+                    onNodeDragStop={onNodeDragStop}
+                    onConnect={onConnect}
                     onSelectionChange={onSelectionChange}
                     fitView
-                    selectionOnDrag
+                    selectionOnDrag={true}
                     minZoom={0.02}
                     defaultViewport={{ x: 0, y: 0, zoom: 1 }}
-                    nodeTypes={nodeTypes}
+                    nodeTypes={nodeTypes} // ✅ use custom node here
+                    onPaneContextMenu={(event) => {
+                        event.preventDefault(); // Prevent browser context menu
+                        setNodes((nds) =>
+                            nds.map((node) => ({
+                                ...node,
+                                selected: false,
+                            }))
+                        );
+                        setEdges((eds) =>
+                            eds.map((edge) => ({
+                                ...edge,
+                                selected: false,
+                            }))
+                        );
+                    }}
                 >
                     <Background />
                     <Controls />
