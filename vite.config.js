7818aa9caa71ebfe82022d11ab86ed55e4809834
Amirkianfar66 <amir.kianfar@limenet.tech>
2025-08-16 14:05:38 +0200

    d

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import svgr from 'vite-plugin-svgr';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
   plugins: [
     react(),
-    svgr(), // ✅ add this
+    svgr({
+      svgrOptions: {
+        icon: true, // optional, adds default width/height props
+      },
+    }),
   ],
 });
