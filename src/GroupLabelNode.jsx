90762b091f48aac34bf3044850f1b77e28651a07
Amirkianfar66 <amir.kianfar@limenet.tech>
2025-08-21 18:02:31 +0200

    5

--- src/GroupLabelNode.jsx
+++ src/GroupLabelNode.jsx
@@ -79,7 +79,7 @@ export default function GroupLabelNode({ id, data }) {
             style={{
                 width: rect.width,
                 height: rect.height,
-                background: "rgba(200,255,255,0.9)",
+                background: "rgba(200,255,255,1)",
                 border: "1px solid red",
                 position: "relative",
                 display: "flex",
