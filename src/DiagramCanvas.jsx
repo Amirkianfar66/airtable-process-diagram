789e2bfc575c5d939af1a8dedf634e142f9e6afb
Amirkianfar66 <amir.kianfar@limenet.tech>
2025-09-03 14:36:26 +0200

    ee3

--- src/DiagramCanvas.jsx
+++ src/DiagramCanvas.jsx
@@ -101,7 +101,7 @@ export default function DiagramCanvas({
             SubUnit: sourceNode.data?.item?.SubUnit || "",
             x: midX,
             y: midY,
-+ edgeId: selectedEdge.id,   // ✅ keep track of parent edge
+            edgeId: selectedEdge.id,   // ✅ keep track of parent edge
     };
 
     const newNode = {
@@ -119,9 +119,9 @@ export default function DiagramCanvas({
     };
 
     setNodes((nds) => [...nds, newNode]);
-    +   if (typeof setItems === "function") {
-        +       setItems((prev) => [...prev, newItem]);
-        +   }
+        if (typeof setItems === "function") {
+                setItems((prev) => [...prev, newItem]);
+            }
 
     setEdges((eds) => [
         ...eds.filter((e) => e.id !== selectedEdge.id),
