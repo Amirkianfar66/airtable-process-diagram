92a2438c36f5b00c36c22f6db67411331affdedd
Amirkianfar66 <amir.kianfar@limenet.tech>
2025-09-05 16:50:18 +0200

    S22

--- src/ProcessDiagram.jsx
+++ src/ProcessDiagram.jsx
@@ -8,6 +8,7 @@ import CustomItemNode from './CustomItemNode';
 import PipeItemNode from './PipeItemNode';
 import ScalableIconNode from './ScalableIconNode';
 import GroupLabelNode from './GroupLabelNode';
+import ItemDetailCard from './ItemDetailCard';
 import GroupDetailCard from './GroupDetailCard';
 import { getItemIcon, handleItemChangeNode, categoryTypeMap } from './IconManager';
 import AIPNIDGenerator, { ChatBox } from './AIPNIDGenerator';
