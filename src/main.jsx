a78da6c7b638a22e9f237cb69597cd87f29603d7
Amirkianfar66 <amir.kianfar@limenet.tech>
2025-10-09 16:13:44 +0200

    ds

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,8 @@
-﻿// src/main.jsx
-import React from "react";
-import { createRoot } from "react-dom/client";
-import { ReactFlowProvider } from "reactflow";
-import AppTabs from "./AppTabs.jsx";
-import "./index.css";
+﻿import React from 'react';
+import ReactDOM from 'react-dom/client';
+import ProcessDiagram from './ProcessDiagram';
+import './index.css';
 
-// (optional) stub so toolbar can switch tabs before AppTabs mounts
-if (typeof window !== "undefined" && typeof window.setAppTab !== "function") {
-    window.setAppTab = (nameOrIndex) => { window.__pendingSetAppTab = nameOrIndex; };
-    window.getAppTab = () => "canvas";
-}
-
-const rootEl = document.getElementById("root");
-if (!rootEl) throw new Error('Missing <div id="root"></div> in index.html');
-
-createRoot(rootEl).render(
-    <ReactFlowProvider>
-        <AppTabs />
-    </ReactFlowProvider>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode><ProcessDiagram /></React.StrictMode>
 );
