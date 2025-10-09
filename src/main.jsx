4d32e383923ec2365b6c28fa33c179949670a640
Amirkianfar66 <amir.kianfar@limenet.tech>
2025-10-09 15:26:37 +0200

    ds

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
-﻿import React from "react";
-import ReactDOM from "react-dom/client";
-import AppTabs from "./AppTabs.jsx";
+﻿import React from 'react';
+import ReactDOM from 'react-dom/client';
+import ProcessDiagram from './ProcessDiagram';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <AppTabs />
-    </React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode><ProcessDiagram /></React.StrictMode>
 );
