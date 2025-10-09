94b7a97f56415d621bb7d6c10ab248990516c9b0
Amirkianfar66 <amir.kianfar@limenet.tech>
2025-10-09 15:36:01 +0200

    fd

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,17 @@
-﻿import React from 'react';
-import ReactDOM from 'react-dom/client';
-import ProcessDiagram from './ProcessDiagram';
-import './index.css';
+﻿// src/main.jsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import AppTabs from "./AppTabs.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode><ProcessDiagram /></React.StrictMode>
+// (optional) super-early stub so toolbar calls are safe pre-mount
+if (typeof window !== "undefined" && typeof window.setAppTab !== "function") {
+    window.setAppTab = (nameOrIndex) => { window.__pendingSetAppTab = nameOrIndex; };
+    window.getAppTab = () => "canvas";
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <AppTabs />
+    </React.StrictMode>
 );
